405041198572ae344f3b8005b7ffce5eed66cbdf
Nathan Essama <emperor.frost17@gmail.com>
2026-09-20 17:13:54 +0000

    did the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import {Sidebar} from './components/Sidebar'
-import './App.css'
+import { Sidebar } from "./components/Sidebar";
+import { Header } from "./components/Header";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
-
-      
+      <main className="main-content">
+        <Header />
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,32 @@
+import "./Header.css";
+
 export function Header() {
-  
+  return (
+    <header className="header">
+      <div className="header__heading">
+        <span className="header__eyebrow">Your money, made clear</span>
+        <h1>Overview</h1>
+      </div>
+
+      <div className="header__actions">
+        <button
+          className="header__notification"
+          type="button"
+          aria-label="Notifications"
+        >
+          <i className="fa-regular fa-bell" aria-hidden="true" />
+          <span className="header__notification-dot" aria-hidden="true" />
+        </button>
+        <span className="header__divider" aria-hidden="true" />
+        <button
+          className="header__profile"
+          type="button"
+          aria-label="Open Maya's profile"
+        >
+          <span className="header__avatar">M</span>
+          <span className="header__name">Maya</span>
+        </button>
+      </div>
+    </header>
+  );
 }
